07920591da11554ec192a82de813d47cbb3dfccb
AndVKuz <139915489+AndVKuz@users.noreply.github.com>
2023-08-24 22:41:55 +0300

    Add files via upload

---
+++ currency_bot/config.py
@@ -0,0 +1,6 @@
+TOKEN = '6677232301:AAGdbMWWcWAESbkFcZAzpDyoGsMazY56uLY'
+keys = {
+    'рубль': 'RUB',
+    'евро': 'EUR',
+    'доллар': 'USD'
+}
---
+++ currency_bot/extensions.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from config import keys
+
+
+class ConvertionException(Exception):
+    pass
+
+
+class CurrencyConverter:
+    @staticmethod
+    def convert(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести одинакове валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {quote}.')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}.')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}.')
+
+        r = requests.get(f'https://v6.exchangerate-api.com/v6/7b5c9a4af3b62974e89c6fd1/pair/{keys[base]}/{keys[quote]}')
+        total_base = json.loads(r.content)['conversion_rate']
+
+        return total_base
